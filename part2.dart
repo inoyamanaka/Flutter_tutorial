94af5a79965105a2ac2f87e9ca3e23cb1e0c62f0
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-03-14 20:02:56 +0700

    Create part2.dart
    Pada bagian ini dikenalkan 2 widget baru yaitu row dan column

---
+++ part2.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Myapp());
+
+// Membuat class Myapp yg merupakan turunan dari kelas StatelessWidget
+// cukup ketikan stl
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan Stateless Widget'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                // Membuat widget Text
+                Text('Nama :'),
+                Text('Herlambang Kurniawan'),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                // Membuat widget Text
+                Text('Alamat :'),
+                Text('Jl. Musktika 3 no.126A'),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                // Membuat widget Text
+                Text('Kota :'),
+                Text('Blora'),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                // Membuat widget Text
+                Text('Kode Pos :'),
+                Text('58212'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
