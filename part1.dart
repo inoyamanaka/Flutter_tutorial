a5b2a0e9b5acb15cec694b8f8106b2566368aa9b
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-03-14 19:14:32 +0700

    Update part1.dart
    - Penambahan Text
    - Beberapa Style

--- part1.dart
+++ part1.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(Myapp());
 
 // Membuat class Myapp yg merupakan turunan dari kelas StatelessWidget
+// cukup ketikan stl
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,7 +12,17 @@ class Myapp extends StatelessWidget {
           appBar: AppBar(
             title: Text("Aplikasi Hello World"),
           ),
-          body: Center(child: Text("Hello world"))),
+          body: Center(
+              child: Container(
+                  color: Color.fromARGB(255, 54, 147, 190),
+                  width: 150,
+                  height: 100,
+                  child: Text(
+                    "Hello Bunyi hari ini mau menonton film apa ya ?",
+                    maxLines: 2,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  )))),
     );
   }
 }
