4feeadb1b1501ca38daf1566404cab90cd4aa009
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-03-21 22:03:45 +0700

    Update part4.dart

--- part4.dart
+++ part4.dart
@@ -14,74 +14,112 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
+          backgroundColor: Color.fromARGB(255, 255, 222, 121),
           appBar: AppBar(
-            title: Center(child: Text("Listile")),
+            title: Center(
+                child: Text(
+              "My Hello ",
+              style: TextStyle(
+                  fontFamily: "BebasNeue",
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            )),
           ),
-          body: ListView(
-            children: [
-              ListTile(
-                contentPadding: EdgeInsets.fromLTRB(35, 30, 35, 35),
-                leading: CircleAvatar(backgroundColor: Colors.lightBlueAccent),
-                title: Text("Uzumaki Naruto",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                subtitle: Text(
+          body: ListView(children: [
+            ChatItem(
+              nama: "Shenhe",
+              Urlimg:
+                  "https://static.wikia.nocookie.net/gensin-impact/images/5/58/Character_Shenhe_Thumb.png/revision/latest?cb=20220210034241",
+              isi:
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                trailing: Text("10.00"),
-              ),
-              Divider(
-                color: Colors.black,
-                thickness: 0.1,
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
-                leading: CircleAvatar(backgroundColor: Colors.lightGreen),
-                subtitle: Text(
+            ),
+            Divider(),
+            ChatItem(
+              nama: "Yoimiya",
+              Urlimg:
+                  "https://static.wikia.nocookie.net/gensin-impact/images/0/05/Character_Yoimiya_Thumb.png/revision/latest/top-crop/width/360/height/360?cb=20220214235604",
+              isi:
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                title: Text("Uzumaki Naruto"),
-                trailing: Text("11.00"),
-              ),
-              Divider(
-                color: Colors.black,
-                thickness: 0.1,
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
-                leading: CircleAvatar(backgroundColor: Colors.amber),
-                subtitle: Text(
+            ),
+            Divider(),
+            ChatItem(
+              nama: "Kazuha",
+              Urlimg: "https://avatarfiles.alphacoders.com/307/307190.png",
+              isi:
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                title: Text("Uzumaki Naruto"),
-                trailing: Text("12.00"),
-              ),
-              Divider(
-                color: Colors.black,
-                thickness: 0.1,
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
-                leading: CircleAvatar(backgroundColor: Colors.redAccent),
-                subtitle: Text(
+            ),
+            Divider(),
+            ChatItem(
+              nama: "Aether",
+              Urlimg: "https://avatarfiles.alphacoders.com/297/297896.png",
+              isi:
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                title: Text("Uzumaki Naruto"),
-                trailing: Text("13.00"),
-              ),
-              Divider(
-                color: Colors.black,
-                thickness: 0.1,
-              ),
-            ],
-          ),
+            ),
+            Divider(),
+            ChatItem(
+              nama: "Lumine",
+              Urlimg:
+                  "https://avatarfiles.alphacoders.com/297/thumb-1920-297897.png",
+              isi:
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            ),
+            Divider(),
+            ChatItem(
+              nama: "Diluc",
+              Urlimg:
+                  "https://avatarfiles.alphacoders.com/280/thumb-280970.png",
+              isi:
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            ),
+            Divider(),
+            ChatItem(
+              nama: "Zhongli",
+              Urlimg:
+                  "https://avatarfiles.alphacoders.com/280/thumb-280958.png",
+              isi:
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            ),
+            Divider(),
+            ChatItem(
+              nama: "Raiden Ei",
+              Urlimg:
+                  "https://avatarfiles.alphacoders.com/307/thumb-307182.png",
+              isi:
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            ),
+            Divider(),
+          ]),
         ));
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String nama;
+  final String Urlimg;
+  final String isi;
+  const ChatItem({this.nama, this.Urlimg, this.isi});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
+      leading: CircleAvatar(
+        radius: 35,
+        backgroundColor: Colors.black,
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(
+            Urlimg,
+          ),
+          radius: 27,
+        ),
+      ),
+      subtitle: Text(
+        isi,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+      ),
+      title: Text(nama, style: TextStyle(fontWeight: FontWeight.bold)),
+      trailing: Text("13.00"),
+    );
   }
 }
