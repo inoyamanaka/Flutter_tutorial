06488e982064878ca5dec295f720a1948ce54f1d
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-03-21 19:15:20 +0700

    Update part4.dart
    - merapikan

--- part4.dart
+++ part4.dart
@@ -9,58 +9,79 @@ void main() {
 // Membuat class Myapp yg merupakan turunan dari kelas StatelessWidget
 // cukup ketikan stl
 class MyApp extends StatelessWidget {
-  final List<Color> balok = [
-    Colors.greenAccent,
-    Color.fromARGB(255, 104, 213, 228),
-    Color.fromARGB(255, 225, 240, 144),
-    Colors.amber,
-    Colors.redAccent,
-    Colors.lime
-  ];
-
-  final List<Widget> text = List.generate(
-      10000,
-      (index) => Text(
-            "HELLO WORLD",
-            textAlign: TextAlign.center,
-            style: TextStyle(height: 7),
-          ));
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            title: Center(
-                child: Text(
-              "Hello",
-              style: TextStyle(
-                fontFamily: 'BebasNeue',
-                fontSize: 30,
-              ),
-            )),
+            title: Center(child: Text("Listile")),
           ),
-          body: ListView.separated(
-            separatorBuilder: (context, index) {
-              return Divider(
-                height: 15,
-                color: Colors.white,
-              );
-            },
-            itemCount: balok.length,
-            itemBuilder: (context, index) {
-              return Container(
-                width: 500,
-                height: 150,
-                color: balok[index],
-                child: text[index],
-              );
-            },
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.start,
+          body: ListView(
+            children: [
+              ListTile(
+                contentPadding: EdgeInsets.fromLTRB(35, 30, 35, 35),
+                leading: CircleAvatar(backgroundColor: Colors.lightBlueAccent),
+                title: Text("Uzumaki Naruto",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                subtitle: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: Text("10.00"),
+              ),
+              Divider(
+                color: Colors.black,
+                thickness: 0.1,
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
+                leading: CircleAvatar(backgroundColor: Colors.lightGreen),
+                subtitle: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                title: Text("Uzumaki Naruto"),
+                trailing: Text("11.00"),
+              ),
+              Divider(
+                color: Colors.black,
+                thickness: 0.1,
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
+                leading: CircleAvatar(backgroundColor: Colors.amber),
+                subtitle: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                title: Text("Uzumaki Naruto"),
+                trailing: Text("12.00"),
+              ),
+              Divider(
+                color: Colors.black,
+                thickness: 0.1,
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
+                leading: CircleAvatar(backgroundColor: Colors.redAccent),
+                subtitle: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                title: Text("Uzumaki Naruto"),
+                trailing: Text("13.00"),
+              ),
+              Divider(
+                color: Colors.black,
+                thickness: 0.1,
+              ),
+            ],
           ),
         ));
   }
 }
-
