ff039ab1634d7cdf70bfb1e788efd8f8f26d0d54
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-03-25 23:19:57 +0700

    Update chat.dart

--- Latihan/chat.dart
+++ Latihan/chat.dart
@@ -1,92 +1,110 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MyApp());
-}
 
 // Membuat class Myapp yg merupakan turunan dari kelas StatelessWidget
 // cukup ketikan stl
-class MyApp extends StatelessWidget {
+class MenuChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Color.fromARGB(255, 255, 222, 121),
-          appBar: AppBar(
-            title: Center(
-                child: Text(
-              "My Hello ",
-              style: TextStyle(
-                  fontFamily: "BebasNeue",
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            )),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomeChat());
+  }
+}
+
+class MyHomeChat extends StatelessWidget {
+  const MyHomeChat({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> MyUser = [
+      {
+        "nama": "Shenhe",
+        "img":
+            "https://static.wikia.nocookie.net/gensin-impact/images/5/58/Character_Shenhe_Thumb.png/revision/latest?cb=20220210034241",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        "nama": "Yoimiya",
+        "img":
+            "https://static.wikia.nocookie.net/gensin-impact/images/0/05/Character_Yoimiya_Thumb.png/revision/latest/top-crop/width/360/height/360?cb=20220214235604",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        "nama": "Kazuha",
+        "img": "https://avatarfiles.alphacoders.com/307/307190.png",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        "nama": "Aether",
+        "img": "https://avatarfiles.alphacoders.com/297/297896.png",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        "nama": "Lumine",
+        "img": "https://avatarfiles.alphacoders.com/297/thumb-1920-297897.png",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        "nama": "Diluc",
+        "img": "https://avatarfiles.alphacoders.com/280/thumb-280970.png",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        "nama": "Zhongli",
+        "img": "https://avatarfiles.alphacoders.com/280/thumb-280958.png",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        "nama": "Raiden Ei",
+        "img": "https://avatarfiles.alphacoders.com/307/thumb-307182.png",
+        "isi":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      }
+    ];
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(123, 220, 181, 1),
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          "My Hello ",
+          style: TextStyle(
+              fontFamily: "BebasNeue",
+              fontSize: 25,
+              fontWeight: FontWeight.bold),
+        )),
+      ),
+      body: ListView(
+          children: MyUser.map((data) {
+        return Dismissible(
+          key: Key("1"),
+          direction: DismissDirection.endToStart,
+          background: Container(
+            color: Colors.red,
+            child: Icon(
+              Icons.delete,
+              size: 25,
+            ),
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.only(right: 20),
           ),
-          body: ListView(children: [
-            ChatItem(
-              nama: "Shenhe",
-              Urlimg:
-                  "https://static.wikia.nocookie.net/gensin-impact/images/5/58/Character_Shenhe_Thumb.png/revision/latest?cb=20220210034241",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          child: Card(
+            color: Colors.grey[850],
+            child: ChatItem(
+              nama: data['nama'],
+              Urlimg: data['img'],
+              isi: data['isi'],
             ),
-            Divider(),
-            ChatItem(
-              nama: "Yoimiya",
-              Urlimg:
-                  "https://static.wikia.nocookie.net/gensin-impact/images/0/05/Character_Yoimiya_Thumb.png/revision/latest/top-crop/width/360/height/360?cb=20220214235604",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            ),
-            Divider(),
-            ChatItem(
-              nama: "Kazuha",
-              Urlimg: "https://avatarfiles.alphacoders.com/307/307190.png",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            ),
-            Divider(),
-            ChatItem(
-              nama: "Aether",
-              Urlimg: "https://avatarfiles.alphacoders.com/297/297896.png",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            ),
-            Divider(),
-            ChatItem(
-              nama: "Lumine",
-              Urlimg:
-                  "https://avatarfiles.alphacoders.com/297/thumb-1920-297897.png",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            ),
-            Divider(),
-            ChatItem(
-              nama: "Diluc",
-              Urlimg:
-                  "https://avatarfiles.alphacoders.com/280/thumb-280970.png",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            ),
-            Divider(),
-            ChatItem(
-              nama: "Zhongli",
-              Urlimg:
-                  "https://avatarfiles.alphacoders.com/280/thumb-280958.png",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            ),
-            Divider(),
-            ChatItem(
-              nama: "Raiden Ei",
-              Urlimg:
-                  "https://avatarfiles.alphacoders.com/307/thumb-307182.png",
-              isi:
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            ),
-            Divider(),
-          ]),
-        ));
+          ),
+        );
+      }).toList()),
+    );
   }
 }
 
@@ -102,11 +120,9 @@ class ChatItem extends StatelessWidget {
       contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 15),
       leading: CircleAvatar(
         radius: 35,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.white,
         child: CircleAvatar(
-          backgroundImage: NetworkImage(
-            Urlimg,
-          ),
+          backgroundImage: NetworkImage(Urlimg),
           radius: 27,
         ),
       ),
@@ -114,10 +130,12 @@ class ChatItem extends StatelessWidget {
         isi,
         maxLines: 2,
         overflow: TextOverflow.ellipsis,
-        style: TextStyle(fontFamily: "AdventPro"),
+        style: TextStyle(fontFamily: "AdventPro", color: Colors.white),
       ),
-      title: Text(nama, style: TextStyle(fontFamily: "Lobster", fontSize: 19)),
-      trailing: Text("13.00"),
+      title: Text(nama,
+          style: TextStyle(
+              fontFamily: "Lobster", fontSize: 19, color: Colors.white)),
+      trailing: Text("13.00", style: TextStyle(color: Colors.white)),
     );
   }
 }
