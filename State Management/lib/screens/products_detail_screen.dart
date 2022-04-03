704bc22f20e0f0775efe2dc0bd7e260b04edff06
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-03 22:13:48 +0700

    Update products_detail_screen.dart

--- State Management/lib/screens/products_detail_screen.dart
+++ State Management/lib/screens/products_detail_screen.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/product_detail.dart';
 
-class ProductDetailScreen extends StatelessWidget {
-  // final String title;
-  // final double price;
-
-  // ProductDetailScreen(this.title, this.price);
-  // static const routeName = '/product-detail';
-
+class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productId =
-        ModalRoute.of(context)?.settings.arguments as String; // is the id!
-    // ...
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Details'),
-      ),
-      body: Center(
-        child: Text("INI ADALAG PAGE PRODUK ($productId)"),
-      ),
+      appBar: AppBar(),
+      body: ProductDetailScreen(),
     );
   }
 }
