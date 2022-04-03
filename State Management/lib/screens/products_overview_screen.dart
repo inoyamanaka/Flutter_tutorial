6ac3d41548c1376340ab649c0bc3cc66282dc0f1
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-03 22:13:33 +0700

    Update products_overview_screen.dart

--- State Management/lib/screens/products_overview_screen.dart
+++ State Management/lib/screens/products_overview_screen.dart
@@ -1,43 +1,12 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/product_grid.dart';
 
-import '../models/product.dart';
-import '../widgets/product_item.dart';
-
-class ProductsOverviewScreen extends StatelessWidget {
-  final List<Product> loadproduct = List.generate(
-    25,
-    (index) {
-      return Product(
-        id: "id_${index + 1}",
-        title: "Product ${index + 1}",
-        description: 'Ini adalah deskripsi produk ${index + 1}',
-        price: 10 + Random().nextInt(100).toDouble(),
-        imageUrl: 'https://picsum.photos/id/$index/200/300',
-      );
-    },
-  );
-
+class ProductOverViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My Soppe"),
-      ),
-      body: GridView.builder(
-          padding: const EdgeInsets.all(10.0),
-          itemCount: loadproduct.length,
-          itemBuilder: (ctx, index) => ProductItem(
-                loadproduct[index].id,
-                loadproduct[index].title,
-                loadproduct[index].imageUrl,
-              ),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          )),
+      appBar: AppBar(title: Text("My Shopee")),
+      body: ProductGridScreen(),
     );
   }
 }
