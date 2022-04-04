b0f4fbce67b443237d8dcb3c4fb69c07f5720e63
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-04 22:05:58 +0700

    Update product_detail.dart

--- State Management/lib/widgets/product_detail.dart
+++ State Management/lib/widgets/product_detail.dart
@@ -5,33 +5,97 @@ import '../providers/all_product.dart';
 class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productId =
-        ModalRoute.of(context)?.settings.arguments as String; // is the id!
+    final productId = ModalRoute.of(context)?.settings.arguments as String;
     final product = Provider.of<Products>(context).findById(productId);
     return Scaffold(
+      // resizeToAvoidBottomInset: false,
       body: Card(
-        color: Colors.amber,
+        // color: Colors.green,
         child: Container(
-          height: 260,
+          color: Colors.white,
+          height: 4000,
           child: Column(
             children: [
-              ListTile(
-                leading: Icon(Icons.album, size: 45),
-                title: Text(product.title),
-              ),
               Container(
                   padding: EdgeInsets.all(10),
                   alignment: Alignment.topLeft,
-                  // color: Colors.black,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15.0),
-                    child: Image.network(
-                      product.UrlImg,
-                      height: 150,
-                      fit: BoxFit.contain,
+                  child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: Image.network(
+                        product.UrlImg,
+                        // height: 150,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   )),
-              Text("Ini merupakan produk yang dibuat oleh lorem ipsum"),
+              Container(
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "${product.title}",
+                  style: TextStyle(
+                      fontFamily: "HappyMonkey",
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  // textAlign: Alignment.topLeft,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.topRight,
+                child: Text(
+                  product.price,
+                  style: TextStyle(
+                      fontFamily: "HappyMonkey",
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  // textAlign: Alignment.topLeft,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.topLeft,
+                child: Text("Laptop",
+                    style: TextStyle(
+                        fontFamily: "HappyMonkey",
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+                alignment: Alignment.topLeft,
+                child: Flexible(
+                  child: Text(
+                    "Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi visual seperti font, tipografi, dan tata letak",
+                    style: TextStyle(
+                        fontFamily: "HappyMonkey",
+                        color: Colors.grey,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(54, 54, 78, 1),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {},
+                ),
+              )
             ],
           ),
         ),
@@ -39,3 +103,5 @@ class ProductDetailScreen extends StatelessWidget {
     );
   }
 }
+
+class TextAlignt {}
