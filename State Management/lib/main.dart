906e41216c2ede650c0a0858226a092d69c86f05
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-04 22:05:09 +0700

    Update main.dart

--- State Management/lib/main.dart
+++ State Management/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/models/product.dart';
 import 'package:flutter_application_1/screens/product_detail_screen.dart';
 import 'package:flutter_application_1/screens/products_overview_screen.dart';
 import 'package:provider/provider.dart';
@@ -11,7 +10,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
