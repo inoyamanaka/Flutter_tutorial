426ce3913651758bf6ab9a59f065464f2805b0c3
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-10 21:59:31 +0700

    Create main.dart

---
+++ Firebase/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../models/akun.dart';
+import 'package:flutter_application_1/pages/register_pages.dart';
+import 'package:flutter_application_1/providers/http_response.dart';
+import 'package:provider/provider.dart';
+
+import '../pages/daftar_akun_pages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => HttpProviderPost(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: RegisterPage(),
+        routes: {'daftar': (context) => DaftarPage()},
+        // theme: TextField(decoration: InputDecoration( fillColor: Colors.red, filled: true)),
+      ),
+    );
+  }
+}
