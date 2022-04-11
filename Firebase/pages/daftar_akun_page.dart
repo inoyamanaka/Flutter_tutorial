8560cac6a04b8e32693bee130d568620edb5b9f3
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-11 22:45:53 +0700

    Update daftar_akun_page.dart

--- Firebase/pages/daftar_akun_page.dart
+++ Firebase/pages/daftar_akun_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/http_response.dart';
@@ -7,21 +8,74 @@ class DaftarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final datas = Provider.of<HttpProviderPost>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Daftar Akun"),
       ),
-      body: ListView.builder(
-        itemCount: datas.jumlahData,
-        itemBuilder: (context, index) {
-          Divider();
-          print(datas.jumlahData);
-          return ListTile(
-            leading: CircleAvatar(backgroundColor: Colors.red),
-            // title: datas['name'],
-          );
-        },
-      ),
+      body: (datas.jumlahData == 0)
+          ? Container(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "No Data",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  SizedBox(height: 20),
+                  OutlinedButton(
+                    onPressed: () {
+                      // Navigator.pushNamed(context, AddPlayer.routeName);
+                    },
+                    child: Text(
+                      "Add Player",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemCount: datas.jumlahData,
+              itemBuilder: (context, index) {
+                var id = datas.data[index].id;
+                // var id = datas.data[index].id;
+                return ListTile(
+                  onTap: () {
+                    // Navigator.pushNamed(
+                    //   context,
+                    //   DetailPlayer.routeName,
+                    //   arguments: id,
+                    // );
+                  },
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmQrNQWaFcfm2MKeA1YqgLmmye6zGeSB3c7mxr1_OPpQ&s"),
+                  ),
+                  title: Text(
+                    "${datas.data[index].name}",
+                  ),
+                  subtitle: Text("${datas.data[index].createdAt}"),
+                  trailing: IconButton(
+                    onPressed: () {
+                      datas.deleteAkun(id.toString()).then(
+                        (_) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("Berhasil dihapus"),
+                              duration: Duration(milliseconds: 500),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
+
