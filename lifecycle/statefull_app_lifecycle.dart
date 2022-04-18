882ec04140a087a89ff938d12309d8db0dd139de
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-18 22:28:50 +0700

    Update statefull_app_lifecycle.dart

--- lifecycle/statefull_app_lifecycle.dart
+++ lifecycle/statefull_app_lifecycle.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/MenuLogin.dart';
-// import './MenuChat.dart';
-// import './MenuLogin.dart';
 
 void main() {
   runApp(MyApp());
