b91514bb5029437a5f898c850163e7c73057740a
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-04-20 23:12:41 +0700

    Update part2.dart

--- part2.dart
+++ part2.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Myapp());
 
-// Membuat class Myapp yg merupakan turunan dari kelas StatelessWidget
-// cukup ketikan stl
+
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
