2e79e2cef8715c98883d198d8317f1c8ce26cb52
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2022-06-30 11:06:04 +0700

    Update main.dart

--- Internet of Things/main.dart
+++ Internet of Things/main.dart
@@ -1,15 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/providers/stat_bar.dart';
-import 'package:flutter_application_1/widget/home_widget.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_screen.dart';
 import 'pages/setting_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late FlutterLocalNotificationsPlugin fltrNotification;
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
